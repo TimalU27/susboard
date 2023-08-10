543ff29791da85ad08963d9a9b458222e6101620
Andrew Lin <landrew051127@gmail.com>
2023-08-10 14:18:57 +0800

    general update
    -fix the vendor id compatibility issue
    -Update VIA definition to V3

--- firmware/susboard/config.h
+++ firmware/susboard/config.h
@@ -4,7 +4,7 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
+#define VENDOR_ID       0x1127
 #define PRODUCT_ID      0x6061
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    timal
